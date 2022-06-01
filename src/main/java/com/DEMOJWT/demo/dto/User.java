2e73c67b8f57d0e7a6129948c0e2e9eef82114fe
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-06-01 12:16:54 -0500

    "Se establece conexion con MongoDB"

--- src/main/java/com/DEMOJWT/demo/dto/User.java
+++ src/main/java/com/DEMOJWT/demo/dto/User.java
@@ -1,9 +1,34 @@
 package com.DEMOJWT.demo.dto;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collation = "usuarios")
 public class User {
+    @Id
+    private Long id;
     private String user;
     private String pwd;
     private String token;
+
+
+    public User() {
+    }
+
+    public User(Long id, String user, String pwd, String token) {
+        this.id = id;
+        this.user = user;
+        this.pwd = pwd;
+        this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUser() {
         return user;
---
+++ src/main/java/com/DEMOJWT/demo/services/UserService.java
@@ -0,0 +1,15 @@
+package com.DEMOJWT.demo.services;
+
+import com.DEMOJWT.demo.dto.User;
+import com.DEMOJWT.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class UserService {
+    @Autowired
+
+    private UserRepository  userRepository;
+    public Flux<User> obtenerUsuario;
+}
